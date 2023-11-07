4911da1306d19be80c63d2ad30c77cfcadda73da
GTexier37 <98769538+GTexier37@users.noreply.github.com>
2023-11-07 14:12:40 +0100

    Update script.js
    fix bug

--- Pire Amie 2/Pire Ami 2-twoPlayers/script.js
+++ Pire Amie 2/Pire Ami 2-twoPlayers/script.js
@@ -1,14 +1,14 @@
-/*Constantes pour la manipulation du DOM*/
+ /*Constantes pour la manipulation du DOM*/
 const mainTitle = document.getElementById('mainTitle');
 const mainContent = document.getElementById('mainContent');
 const firstButton = document.getElementById('button');
 const words = document.querySelector('#word');
-const indices = document.getElementById('indice');
-const howManyIndice = document.getElementById('howManyIndice');
+const clue = document.getElementById('clue');
+const howManyClue = document.getElementById('howManyClue');
 const winOrLoose = document.getElementById('winOrLoose');
 
 /*Tableau de mots à faire deviner*/
-const mots = [
+const wordTab = [
   "Chat", "Chien", "Maison", "Voiture", "Ordinateur", "Téléphone", "Arbre", "Fleur", "Livre", "Musique",
   "Plage", "Montagne", "Étoile", "Lune", "Soleil", "Bonheur", "Amour", "Famille", "Ami", "Cuisine",
   "Restaurant", "Voyage", "Avion", "Train", "Bateau", "École", "Université", "Art", "Sport", "Nature",
@@ -16,42 +16,42 @@ const mots = [
   "Histoire", "Géographie", "Langage", "Bibliothèque", "Photographie", "Tableau", "Peinture", "Sculpture",
   "Danse", "Chanson"
 ];
-
+/*Variables*/
 let wordAlreadyUsed = [];
 
-let intHowManyIndice;
+let intHowManyClue;
 
 
 //---------------------------------------------------------------------------------
 
 
-/*Fonction pour choisir un mot "aléatoire" dans le tableau mots*/
-/*Cela créer un bouton qui lance la fonction nbrHint (choix du nombre d'indice)*/
+/*Fonction pour choisir un mot "aléatoire" dans le tableau wordTab*/
+/*Cela créer un bouton qui lance la fonction nbrClue (choix du nombre d'indice)*/
 function randomWord(){
    mainTitle.innerHTML = "Le Pire Ami 2 A 2";
-   howManyIndice.style.visibility ='hidden';
+   howManyClue.style.visibility ='hidden';
    firstButton.style.visibility = 'visible';
-   let aleatoire = Math.floor(Math.random()*mots.length);
-   if(wordAlreadyUsed.includes(aleatoire)){
+   let random = Math.floor(Math.random()*wordTab.length);
+   if(wordAlreadyUsed.includes(random)){
      randomWord();
    }else{
-     words.innerHTML = `Le mot à faire deviner est :<br\/> ${mots[aleatoire]}`;
-     indices.innerHTML = "<button onclick='nbrHint()'> Clic pour donner le nombre d'indice</button>";
+     words.innerHTML = `Le mot à faire deviner est :<br\/> ${wordTab[random]}`;
+     clue.innerHTML = "<button onclick='nbrClue()'> Clic pour donner le nombre d'indice</button>";
      winOrLoose.innerHTML = "";
    }
-   wordAlreadyUsed.push(aleatoire);
+   wordAlreadyUsed.push(random);
    wordAlreadyUsed.sort(); 
    console.log(wordAlreadyUsed);
 }
 
 /*Fonction qui me demande le nombre d'indice à donner et récupère la valeur saisie*/
 /*Cela lance la fonction indice*/
-function nbrHint(){
-  indices.innerHTML = "";
-  howManyIndice.style.visibility = 'visible';
-  howManyIndice.addEventListener("keydown", function(event){
+function nbrClue(){
+  clue.innerHTML = "";
+  howManyClue.style.visibility = 'visible';
+  howManyClue.addEventListener("keydown", function(event){
     if(event.key === "Enter"){
-      intHowManyIndice = howManyIndice.value;
+      intHowManyClue = howManyClue.value;
       indice()
     }
   });
@@ -60,13 +60,13 @@ function nbrHint(){
 /*Fonction qui verifie si la valeur saisie dans nbrHint est conforme et repond en fonction de la valeur*/
 /*Si c'est un chiffre entre 1 et 10* cela créer un bouton qui lance la fonction boucleIndice*/
 function indice(){
-  howManyIndice.style.visibility ='hidden';
-  if(intHowManyIndice >= 1 && intHowManyIndice <= 10){
-    indices.innerHTML= `Il te reste ${intHowManyIndice} indices à donner <br\/> <br\/> <button class='tour' onclick='boucleIndice()'>Clic pour passer au tour suivant</button>`;
-  }else if(intHowManyIndice > 10){
-    indices.innerHTML ="Il y a un peu trop d'indices, non ? <br\/> Indique un chiffre entre 1 et 10 <br\/> <br\/> <button onclick='nbrHint()'> Clic pour donner le nombre d'indice</button>";
+  howManyClue.style.visibility ='hidden';
+  if(intHowManyClue >= 1 && intHowManyClue <= 10){
+    clue.innerHTML= `Il te reste ${intHowManyClue} indices à donner <br\/> <br\/> <button class='tour' onclick='boucleIndice()'>Clic pour passer au tour suivant</button>`;
+  }else if(intHowManyClue > 10){
+    clue.innerHTML ="Il y a un peu trop d'indices, non ? <br\/> Indique un chiffre entre 1 et 10 <br\/> <br\/> <button onclick='nbrClue()'> Clic pour donner le nombre d'indice</button>";
     }else{
-      indices.innerHTML = "Tu dois mentionner un chiffre entre 1 et 10 ! <br\/> <br\/> <button onclick='nbrHint()'> Clic pour donner le nombre d'indice</button>";
+      clue.innerHTML = "Tu dois mentionner un chiffre entre 1 et 10 ! <br\/> <br\/> <button onclick='nbrClue()'> Clic pour donner le nombre d'indice</button>";
     }
 }
 
@@ -74,12 +74,12 @@ function indice(){
 /*Em même temps un bouton gagné et perdu sont créer et lance les fontions win ou loose*/
 function boucleIndice(){
   
-  intHowManyIndice -= 1;
-  if(intHowManyIndice === 0 || intHowManyIndice < 0){
-    indices.innerHTML = "Tu n'as plus d'indice en reserve";
+  intHowManyClue -= 1;
+  if(intHowManyClue === 0 || intHowManyClue < 0){
+    clue.innerHTML = "Tu n'as plus d'indice en reserve";
     winOrLoose.innerHTML = "<button onclick='win()'>Gagné ?</button> ou <button onclick='loose()'>Perdu ?</button>";
   }else{
-    indices.innerHTML= `Il te reste ${intHowManyIndice} indices à donner <br\/> <br\/> <button onclick='boucleIndice()'>Clic pour passer au tour suivant</button>`;
+    clue.innerHTML= `Il te reste ${intHowManyClue} indices à donner <br\/> <br\/> <button onclick='boucleIndice()'>Clic pour passer au tour suivant</button>`;
     winOrLoose.innerHTML = "<button onclick='win()'>Gagné ?</button>";
   }
 }
@@ -88,7 +88,7 @@ function win(){
   mainTitle.innerHTML = "Gagné !!! <br\/><br\/> <button onclick='randomWord()'> Nouvelle Partie ?</button>";
   firstButton.style.visibility ='hidden';
   words.innerHTML = "";
-  indices.innerHTML = "";
+  clue.innerHTML = "";
   winOrLoose.innerHTML= "";
 }
 
@@ -97,6 +97,6 @@ function loose(){
   mainTitle.innerHTML = "Perdu !!! <br\/><br\/> <button onclick='randomWord()'> Nouvelle Partie ?</button>";
   firstButton.style.visibility ='hidden';
   words.innerHTML = "";
-  indices.innerHTML = "";
+  clue.innerHTML = "";
   winOrLoose.innerHTML= "";
 }
